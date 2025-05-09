8cb7973d0926406108630f584db95a59db9527f3
AbdallahSaeed0 <bodyahly17@gmail.com>
2025-05-09 20:59:07 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,7 +10,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(
-            SettingsSeeder::class,
+            SettingsSeeder::class
         );
     }
 }
